1738fd48d78724b6c357854d63bab153a2f73dac
Agissni <kaylanajwa674@gmail.com>
2026-05-06 08:47:22 +0700

    Implement Bubble Sort algorithm in BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,36 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // tukar
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // optimasi: berhenti jika sudah tidak ada pertukaran
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {5, 2, 9, 1, 3};
+
+        bubbleSort(data);
+
+        System.out.print("Hasil sorting: ");
+        for (int i : data) {
+            System.out.print(i + " ");
+        }
+    }
+}
